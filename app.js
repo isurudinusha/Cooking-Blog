b8a497083b774ac8cd95cd60b205dacd362e63c7
Isuru Dinusha <isurudinusha@gmail.com>
2023-05-15 15:30:09 +0100

    UI Update

--- app.js
+++ app.js
@@ -27,10 +27,12 @@ app.use(fileUpload());
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-
-
 const routes = require("./server/routes/recipeRoutes.js");
 app.use("/", routes);
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
